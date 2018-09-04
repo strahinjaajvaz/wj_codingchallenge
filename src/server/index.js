75eaa7890d8abd19aaeeed10ca64e53351b18ceb
Strahinja Ajvaz <strahinja@dreamwalk.com>
2018-09-05 08:01:32 +1000

    added dynamic port

--- src/server/index.js
+++ src/server/index.js
@@ -70,6 +70,8 @@ app.get("*", (req, res, next) => {
       `);
     });
 });
-app.listen(3000, () => {
-  console.log("server is listening on port: 3000");
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`server is listening on port: ${PORT}`);
 });
